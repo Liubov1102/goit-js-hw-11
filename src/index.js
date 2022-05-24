5e997210cb323d419773a08c22eb629abda782e8
Liubov1102 <laav.odessa@gmail.com>
2022-05-24 17:43:53 +0300

    fixed
    fixed

--- src/index.js
+++ src/index.js
@@ -76,3 +76,15 @@ window.addEventListener('scroll', () => {
     }
 });
 
+const scrollBtn = document.querySelector('.isShowBtn')
+window.onscroll = () => {
+    if (window.scrollY > 700) {
+        scrollBtn.classList.remove('isShowBtn_hide');
+        if (window.scrollY < 700) {
+          scrollBtn.classList.add('isShowBtn_hide');  
+        }
+    }
+}
+scrollBtn.onclick = () => {
+    window.scrollTo(0, 0);
+}
